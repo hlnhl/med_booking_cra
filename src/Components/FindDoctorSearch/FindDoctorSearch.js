640d5560a7bfc2b9d5ee457ce4312585eb2bebc7
hlnhl <hoelhelen@gmail.com>
2025-03-13 14:07:11 -0700

    updated DoctorSearch css

--- src/Components/FindDoctorSearch/FindDoctorSearch.js
+++ src/Components/FindDoctorSearch/FindDoctorSearch.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import classes from './FindDoctorSearch.module.css';
 
 const initSpeciality = [
@@ -23,19 +23,18 @@ const FindDoctorSearch = () => {
             <div className={classes.search_image_div}>
                 <img className={classes.search_image} src="dr_search.png" alt="" />
             </div>
-            <div className={classes.search_title_div}>
+            <div className={classes.search_div}>
                 <h1 className={classes.search_h1}>Find a doctor</h1>
                 <h1 className={`${classes.search_h1} ${classes.gradient}`}>at your own ease</h1>
                 
                 <div className={classes.search_box}>
-                    {/* <p>Perform a search to see the results.</p> */}
 
                     <input type="text" className={classes.search_input} placeholder="Search doctors by specialty..." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
 
                     <div className={classes.search_results} hidden={doctorResultHidden}>
                         {
                             specialities.map(speciality => <div className={classes.search_result_item} key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
-                                <span></span>
+                                <span><img className={classes.search_icon} src="search.svg" alt="magnifying glass icon" /></span>
                                 <span>{speciality}</span>
                                 <span>SPECIALITY</span>
                             </div>)
