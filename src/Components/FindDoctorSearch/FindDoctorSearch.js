b3842c816ba69064cdd39754d5a64ed8ff121aea
hlnhl <hoelhelen@gmail.com>
2025-03-13 12:02:22 -0700

    working on text input search logic

--- src/Components/FindDoctorSearch/FindDoctorSearch.js
+++ src/Components/FindDoctorSearch/FindDoctorSearch.js
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, Navigate } from 'react-router-dom';
 import classes from './FindDoctorSearch.module.css';
 
-const initSpeciality = [
-    'General Physician', 'Cardiologist', 'Dentist', 'Dermatologist', 'Otolaryngologist (ENT)', 'Endocrinologist', 'Gynecologist/Obstetrician', 'Neurologist', 'Oncologist', 'Orthopedist', 'Pediatrician', 'Physical Therapist', 'Podiatrist', 'Pulmonologist', 'Rheumatologist'
-]
-
 const FindDoctorSearch = () => {
     const [doctorResultHidden, setDoctorResultHidden] = useState(true);
+    const [searchItem, setSearchItem] = useState('');
     const [searchDoctor, setSearchDoctor] = useState('');
-    const [specialities, setSpecialities] = useState(initSpeciality);
+    const [specialities, setSpecialities] = useState([]);
+    const [filteredSpecialities, setFilteredSpecialities] = useState([]);
+
+    useEffect(() => {
+        fetch('dr_list.json')
+        .then(res => res.json())
+        .then(data => {
+            setSpecialities(data.speciality);
+            setFilteredSpecialities(data.speciality);
+        })
+        .catch(err => console.log(err));
+    }, [])
+
     const navigate = useNavigate();
+
+    const handleInputChange = (e) => {
+        const searchTerm = e.target.value;
+        setSearchItem(searchTerm);
+    }
+
     const handleDoctorSelect = (speciality) => {
         setSearchDoctor(speciality);
         setDoctorResultHidden(true);
@@ -30,11 +45,11 @@ const FindDoctorSearch = () => {
                 <div className={classes.search_box}>
                     {/* <p>Perform a search to see the results.</p> */}
 
-                    <input type="text" className={classes.search_input} placeholder="Search doctors by specialty..." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchDoctor} onChange={(e) => setSearchDoctor(e.target.value)} />
+                    <input type="text" className={classes.search_input} placeholder="Search doctors by specialty..." onFocus={() => setDoctorResultHidden(false)} onBlur={() => setDoctorResultHidden(true)} value={searchItem} onChange={(e) => setSearchDoctor(e.target.value)} />
 
                     <div className={classes.search_results} hidden={doctorResultHidden}>
                         {
-                            specialities.map(speciality => <div className={classes.search_result_item} key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
+                            filteredSpecialities.map(speciality => <div className={classes.search_result_item} key={speciality} onMouseDown={() => handleDoctorSelect(speciality)}>
                                 <span></span>
                                 <span>{speciality}</span>
                                 <span>SPECIALITY</span>
